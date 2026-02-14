64c45c0f9fcf0bd4f9d9c534f7a88b4a8562fcab
parafino01 <113385659+parafino01@users.noreply.github.com>
2026-02-14 16:34:11 +0000

    Create MainActivity.java

---
+++ app/src/main/java/com/tb/app/MainActivity.java
@@ -0,0 +1,24 @@
+package com.seunome.app;
+
+import android.os.Bundle;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    WebView webView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        webView = findViewById(R.id.webview);
+        webView.getSettings().setJavaScriptEnabled(true);
+        webView.setWebViewClient(new WebViewClient());
+
+        // Carrega o arquivo index.html dos assets
+        webView.loadUrl("file:///android_asset/index.html");
+    }
+}
